65d58f0da19e8a52c9da522c765f06567cbd4827
Krayon <krayon.git@qdnx.org>
2018-08-12 13:48:05 +1000

    Hide cursor
    Signed-off-by: Krayon <krayon.git@qdnx.org>

--- game/root.gd
+++ game/root.gd
@@ -2,14 +2,16 @@
 
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func _input(ev):
+	if (ev.is_pressed() and ev.is_action("ui_cancel")):
+		get_tree().quit()
+		return
+
+func _exit_tree():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
