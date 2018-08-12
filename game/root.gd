7c0a50abfa6f76dba0b91df5d73468cc776990fa
Krayon <krayon.git@qdnx.org>
2018-08-12 13:44:04 +1000

    Add initial Godot3 Project
    Signed-off-by: Krayon <krayon.git@qdnx.org>

---
+++ game/root.gd
@@ -0,0 +1,17 @@
+# vim:ts=4:tw=80:sw=4:ai:si
+
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
